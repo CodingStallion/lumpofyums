3268f21686827952143f3c1b9d117e57ced79ca1
eiithan <eithan.rb@gmail.com>
2022-01-24 10:14:06 +0800

    Added Delete button and changed redirect for create Recipe
    - only appears when user is logged in (to be improved).
    - Upon creating recipe, user is redirected to home page

--- src/main/java/RecipeServlet.java
+++ src/main/java/RecipeServlet.java
@@ -1,7 +1,5 @@
 
-
 import java.io.IOException;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import lumpofyums.Recipe;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -26,146 +23,169 @@ import javax.servlet.RequestDispatcher;
 @WebServlet("/RecipeServlet")
 public class RecipeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	//Step 1: Prepare list of variables used for database connections
-		 private String jdbcURL = "jdbc:mysql://localhost:3306/lump_of_yums";
-		 private String jdbcUsername = "root";
-		 private String jdbcPassword = "password";
-		 
-	//Step 2: Prepare list of SQL prepared statements to perform CRUD to our database
-		 private static final String INSERT_RECIPE_SQL = "INSERT INTO RECIPE" + " (food_name, prep_time, cooking_time, level, description, ingredients, preparation, uid) VALUES " +" (?,?,?,?,?,?,?,?);";
-		 private static final String SELECT_RECIPE_BY_ID = "select food_name, prep_time, cooking_time, level, description, ingredients, preparation, uid, username from recipe R INNER JOIN user U ON R.uid = U.id where food_name =?";
-		 private static final String SELECT_ALL_RECIPE = "select * from recipe R INNER JOIN user U ON R.uid = U.id";
-		 private static final String DELETE_RECIPE_SQL = "delete from recipe where food_name = ?;";
-		 //private static final String UPDATE_RECIPE_SQL = "update recipe set name = ?,password= ?, email =?,language =? where name = ?;";
-		 
-	//Step 3: Implement the getConnection method which facilitates connection to the database via JDBC
-		  protected Connection getConnection() {
-		  Connection connection = null;
-		  try {
-		  Class.forName("com.mysql.cj.jdbc.Driver");
-		  connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
-		  } catch (SQLException e) {
-		  e.printStackTrace();
-		  } catch (ClassNotFoundException e) {
-		  e.printStackTrace();
-		  }
-		  return connection;
-		  }
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecipeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	// Step 1: Prepare list of variables used for database connections
+	private String jdbcURL = "jdbc:mysql://localhost:3306/lump_of_yums";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "password";
+
+	// Step 2: Prepare list of SQL prepared statements to perform CRUD to our
+	// database
+	private static final String INSERT_RECIPE_SQL = "INSERT INTO RECIPE"
+			+ " (food_name, prep_time, cooking_time, level, description, ingredients, preparation, uid) VALUES "
+			+ " (?,?,?,?,?,?,?,?);";
+	private static final String SELECT_RECIPE_BY_ID = "select food_name, prep_time, cooking_time, level, description, ingredients, preparation, uid, username from recipe R INNER JOIN user U ON R.uid = U.id where food_name =?";
+	private static final String SELECT_ALL_RECIPE = "select * from recipe R INNER JOIN user U ON R.uid = U.id";
+	private static final String DELETE_RECIPE_SQL = "delete from recipe where food_name = ?;";
+	private static final String UPDATE_RECIPE_SQL = "update recipe set food_name = ?,prep_time= ?, cooking_time =?,description =?,ingredients =?,preparation =?,uid =? where food_name = ?;";
+
+	// private static final String UPDATE_RECIPE_SQL = "update recipe set name =
+	// ?,password= ?, email =?,language =? where name = ?;";
+
+	// Step 3: Implement the getConnection method which facilitates connection to
+	// the database via JDBC
+	protected Connection getConnection() {
+		Connection connection = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public RecipeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
-		//Step 4: Depending on the request servlet path, determine the function to invoke using the follow switch statement.
+
+		// Step 4: Depending on the request servlet path, determine the function to
+		// invoke using the follow switch statement.
 		String action = request.getServletPath();
-		 try {
-		 switch (action) {
-		 case "/RecipeServlet/recipe":
-			showRecipe(request, response);
-		 break;
-		 case "/RecipeServlet/home":
-		 listUsers(request, response);
-		 break;
-		 }
-		 } catch (SQLException ex) {
-		 throw new ServletException(ex);
-		 }
+		try {
+			switch (action) {
+			case "/RecipeServlet/recipe":
+				showRecipe(request, response);
+				break;
+			case "/RecipeServlet/home":
+				listUsers(request, response);
+				break;
+			case "/RecipeServlet/delete":
+				deleteRecipe(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
 
-		
-		
 	}
-	
-	//Step 5: listUsers function to connect to the database and retrieve all users records
+
+	// Step 5: listUsers function to connect to the database and retrieve all users
+	// records
 	private void listUsers(HttpServletRequest request, HttpServletResponse response)
-	throws SQLException, IOException, ServletException 
-	{
-	List <Recipe> recipes = new ArrayList <>();
-	 try (Connection connection = getConnection();
-	 // Step 5.1: Create a statement using connection object
-	 PreparedStatement preparedStatement = 
-	connection.prepareStatement(SELECT_ALL_RECIPE);) {
-	 // Step 5.2: Execute the query or update query
-	 ResultSet rs = preparedStatement.executeQuery();
-	 // Step 5.3: Process the ResultSet object.
-	 while (rs.next()) {
-		 String food_name = rs.getString("food_name");
-		 int prep_time = rs.getInt("prep_time");
-		 int cooking_time = rs.getInt("cooking_time");
-		 String level = rs.getString("level");
-		 String description = rs.getString("description");
-		 String ingredients = rs.getString("ingredients");
-		 String preparation = rs.getString("preparation");
-		 int uid = rs.getInt("uid");
-		 String username= rs.getString("username");
+			throws SQLException, IOException, ServletException {
+		List<Recipe> recipes = new ArrayList<>();
+		try (Connection connection = getConnection();
+				// Step 5.1: Create a statement using connection object
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_RECIPE);) {
+			// Step 5.2: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+			// Step 5.3: Process the ResultSet object.
+			while (rs.next()) {
+				String food_name = rs.getString("food_name");
+				int prep_time = rs.getInt("prep_time");
+				int cooking_time = rs.getInt("cooking_time");
+				String level = rs.getString("level");
+				String description = rs.getString("description");
+				String ingredients = rs.getString("ingredients");
+				String preparation = rs.getString("preparation");
+				int uid = rs.getInt("uid");
+				String username = rs.getString("username");
 
-		 recipes.add(new Recipe(food_name, prep_time, cooking_time, level, description, ingredients, preparation, uid, username));
-	 }
-	 } catch (SQLException e) {
-	 System.out.println(e.getMessage());
-	 }
-	// Step 5.4: Set the users list into the listUsers attribute to be pass to the userManagement.jsp
-	request.setAttribute("listUsers", recipes);
-	request.getRequestDispatcher("/index.jsp").forward(request, response);
-	
-	
+				recipes.add(new Recipe(food_name, prep_time, cooking_time, level, description, ingredients, preparation,
+						uid, username));
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		// Step 5.4: Set the users list into the listUsers attribute to be pass to the
+		// userManagement.jsp
+		request.setAttribute("listUsers", recipes);
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
+
 	}
-	
-	
-	//method to get parameter, query database for existing user data and redirect to user edit page
+
+	// method to get parameter, query database for existing user data and redirect
+	// to user edit page
 	private void showRecipe(HttpServletRequest request, HttpServletResponse response)
-	throws SQLException, ServletException, IOException {
-	//get parameter passed in the URL
-	String food_name = request.getParameter("food_name");
-	Recipe existingRecipe = new Recipe("",1,1,"","","","",1,"");
-	// Step 1: Establishing a Connection
-	try (Connection connection = getConnection();
-	// Step 2:Create a statement using connection object
-	PreparedStatement preparedStatement = 
-	connection.prepareStatement(SELECT_RECIPE_BY_ID);) {
-	preparedStatement.setString(1, food_name);
-	// Step 3: Execute the query or update query
-	ResultSet rs = preparedStatement.executeQuery();
-	// Step 4: Process the ResultSet object 
-	while (rs.next()) {
-		 food_name = rs.getString("food_name");
-		 int prep_time = rs.getInt("prep_time");
-		 int cooking_time = rs.getInt("cooking_time");
-		 String level = rs.getString("level");
-		 String description = rs.getString("description");
-		 String ingredients = rs.getString("ingredients");
-		 String preparation = rs.getString("preparation");
-		 int uid = rs.getInt("uid");
-		 String username= rs.getString("username");
-		 existingRecipe = new Recipe(food_name, prep_time, cooking_time, level, description, ingredients, preparation, uid, username);
+			throws SQLException, ServletException, IOException {
+		// get parameter passed in the URL
+		String food_name = request.getParameter("food_name");
+		Recipe existingRecipe = new Recipe("", 1, 1, "", "", "", "", 1, "");
+		// Step 1: Establishing a Connection
+		try (Connection connection = getConnection();
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection.prepareStatement(SELECT_RECIPE_BY_ID);) {
+			preparedStatement.setString(1, food_name);
+			// Step 3: Execute the query or update query
+			ResultSet rs = preparedStatement.executeQuery();
+			// Step 4: Process the ResultSet object
+			while (rs.next()) {
+				food_name = rs.getString("food_name");
+				int prep_time = rs.getInt("prep_time");
+				int cooking_time = rs.getInt("cooking_time");
+				String level = rs.getString("level");
+				String description = rs.getString("description");
+				String ingredients = rs.getString("ingredients");
+				String preparation = rs.getString("preparation");
+				int uid = rs.getInt("uid");
+				String username = rs.getString("username");
+				existingRecipe = new Recipe(food_name, prep_time, cooking_time, level, description, ingredients,
+						preparation, uid, username);
 
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		// Step 5: Set existingUser to request and serve up the userEdit form
+		request.setAttribute("recipe", existingRecipe);
+		request.getRequestDispatcher("/Recipe.jsp").forward(request, response);
 	}
-	} catch (SQLException e) {
-	System.out.println(e.getMessage());
-	}
-	//Step 5: Set existingUser to request and serve up the userEdit form
-	request.setAttribute("recipe", existingRecipe);
-	request.getRequestDispatcher("/Recipe.jsp").forward(request, response);
-	}
-	
-	
 
-
+	private void deleteRecipe(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException {
+		// Step 1: Retrieve value from the request
+		String food_name = request.getParameter("food_name");
+		// Step 2: Attempt connection with database and execute delete user SQL query
+		try (Connection connection = getConnection();
+				PreparedStatement statement = connection.prepareStatement(DELETE_RECIPE_SQL);) {
+			statement.setString(1, food_name);
+			int i = statement.executeUpdate();
+		}
+		// Step 3: redirect back to RecipeServlet dashboard
+		response.sendRedirect("http://localhost:8090/lumpofyums/RecipeServlet/home");
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
--- src/main/webapp/Recipe.jsp
+++ src/main/webapp/Recipe.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,83 +8,111 @@
 <!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Cookie&family=Mochiy+Pop+P+One&display=swap" rel="stylesheet">
+<link
+	href="https://fonts.googleapis.com/css2?family=Cookie&family=Mochiy+Pop+P+One&display=swap"
+	rel="stylesheet">
 
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<link rel="stylesheet" 
-href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
-integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
-crossorigin="anonymous">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-<link rel="stylesheet" href="${pageContext.request.contextPath}/css/style.css">
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+	crossorigin="anonymous">
+<link rel="stylesheet"
+	href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+<link rel="stylesheet"
+	href="${pageContext.request.contextPath}/css/style.css">
 <title>Insert title here</title>
 </head>
-<body>    
+<body>
 
-<nav class="nav-wrapper">
-    
-      <a href="<%=request.getContextPath()%>/RecipeServlet/home"  class="brand-logo">Lump Of Yums</a>
-      <ul class="nav-list">
-        <li class="active"><a href="<%=request.getContextPath()%>/RecipeServlet/home" >Recipes</a></li>
-            <%
-                                   
-                if (session.getAttribute("logUser") == null) {
-            %>
-        <li><a href="<%=request.getContextPath()%>/login.jsp">Sign in</a></li>
-        <li><a href="<%=request.getContextPath()%>/register.jsp">Sign up</a></li>
-        <% } else {
-         %>
-         	<li><a href="<%=request.getContextPath()%>/account.jsp">Account</a></li>
-            <li><a href="LogoutServlet">Logout</a></li>
-        <% }%>
-      </ul>
-    </nav>
-    <section class="bg-orange main-section">
-      <div class="restaurant-header">
-      <c:if test="${recipe != null}">
-        <h1 class="text-brown" id="name"><c:out value="${recipe.food_name}" /></h1>
-        </c:if>
-        <div class="restaurant-info">
-          <div class="rating" style="color:black;">Preparation time: <c:out value="${recipe.prep_time}" /> Minutes</div>
-          <div class="price" id="price">Cooking time: <c:out value="${recipe.cooking_time}" /> Minutes</div>
-          <div class="price" id="price">Level: <c:out value="${recipe.level}" /></div>
-        </div>
-         <div class="mb-medium">
-          <p class="address"><c:out value="Posted by: ${recipe.username}" /></p>
-        </div>
-      </div>
-      <div class="restaurant-details">
-        <h1 class="text-brown mb-small">Content</h1>
-        <div class="double-panel restaurant-details-content">
-          <div class="left" style="width: 70%;">
-            <div>
-              <i class="material-icons">description</i>
-              <span class="address"><c:out value="${recipe.description}" /></span>
-               </div>
-            <div>
-              <i class="material-icons">menu_book</i>
-              <span id="cuisine"><c:out value="${recipe.ingredients}" /></span>
-            </div>
-           
-          </div>
-          <div class="left">
-          <div>
-           <i class="material-icons">menu_book</i>
-              <span id="cuisine"><c:out value="${recipe.preparation}" /></span>
-          </div>
-         
-          </div>
-        </div>
-      </div>
-      <div class="restaurant-reviews">
-        <h1 class="text-brown mb-small">Reviews</h1>
-        <a class="btn" id="btn-review">Click here</a>
-      </div>
-    </section>
-    
-        <footer class="footer">
-      <h4><i class="material-icons">dining</i></h4>
-      <span>&copy; Lump Of Yums 2022</span>
-    </footer>
+	<nav class="nav-wrapper">
+
+		<a href="<%=request.getContextPath()%>/RecipeServlet/home"
+			class="brand-logo">Lump Of Yums</a>
+		<ul class="nav-list">
+			<li class="active"><a
+				href="<%=request.getContextPath()%>/RecipeServlet/home">Recipes</a></li>
+			<%
+			if (session.getAttribute("logUser") == null) {
+			%>
+			<li><a href="<%=request.getContextPath()%>/login.jsp">Sign
+					in</a></li>
+			<li><a href="<%=request.getContextPath()%>/register.jsp">Sign
+					up</a></li>
+			<%
+			} else {
+			%>
+			<li><a href="<%=request.getContextPath()%>/account.jsp">Account</a></li>
+			<li><a href="LogoutServlet">Logout</a></li>
+			<%
+			}
+			%>
+		</ul>
+	</nav>
+	<section class="bg-orange main-section">
+		<div class="restaurant-header">
+			<c:if test="${recipe != null}">
+				<h1 class="text-brown" id="name">
+					<c:out value="${recipe.food_name}" />
+				</h1>
+			</c:if>
+			<div class="restaurant-info">
+				<div class="rating" style="color: black;">
+					Preparation time:
+					<c:out value="${recipe.prep_time}" />
+					Minutes
+				</div>
+				<div class="price" id="price">
+					Cooking time:
+					<c:out value="${recipe.cooking_time}" />
+					Minutes
+				</div>
+				<div class="price" id="price">
+					Level:
+					<c:out value="${recipe.level}" />
+				</div>
+			</div>
+			<div class="mb-medium">
+				<p class="address">
+					<c:out value="Posted by: ${recipe.username}" />
+				</p>
+			</div>
+		</div>
+		<div class="restaurant-details">
+			<h1 class="text-brown mb-small">Content</h1>
+			<div class="double-panel restaurant-details-content">
+				<div class="left" style="width: 70%;">
+					<div>
+						<i class="material-icons">description</i> <span class="address"><c:out
+								value="${recipe.description}" /></span>
+					</div>
+					<div>
+						<i class="material-icons">menu_book</i> <span id="cuisine"><c:out
+								value="${recipe.ingredients}" /></span>
+					</div>
+
+				</div>
+				<div class="left">
+					<div>
+						<i class="material-icons">menu_book</i> <span id="cuisine"><c:out
+								value="${recipe.preparation}" /></span>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="restaurant-reviews">
+			<h1 class="text-brown mb-small">Reviews</h1>
+			<a class="btn" id="btn-review">Click here</a> <a
+				href="edit?food_name=<c:out value='${recipe.food_name}' />">Edit</a>
+		</div>
+	</section>
+
+	<footer class="footer">
+		<h4>
+			<i class="material-icons">dining</i>
+		</h4>
+		<span>&copy; Lump Of Yums 2022</span>
+	</footer>
 </body>
 </html>
